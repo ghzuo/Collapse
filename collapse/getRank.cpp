93739bcb79e8c4ff097e4cf4fda7f3be51c1c633
Guanghong Zuo <ghzuo@fudan.edu.cn>
2022-03-26 23:38:54 +0800

    output all inbuit rank in rank token

--- collapse/getRank.cpp
+++ collapse/getRank.cpp
@@ -7,7 +7,7 @@
  * @Author: Dr. Guanghong Zuo
  * @Date: 2022-03-22 15:10:34
  * @Last Modified By: Dr. Guanghong Zuo
- * @Last Modified Time: 2022-03-26 14:00:30
+ * @Last Modified Time: 2022-03-26 23:38:00
  */
 
 #include "getRank.h"
@@ -16,12 +16,16 @@ void getRank(int argc, char *argv[]) {
   // get the name of file
   string outfile("ranklist.txt");
   string program(argv[0]);
+  bool allrank(false);
 
   char ch;
-  while ((ch = getopt(argc, argv, "o:qh")) != -1) {
+  while ((ch = getopt(argc, argv, "o:aqh")) != -1) {
     switch (ch) {
     case 'o':
       outfile = optarg;
+      break;
+    case 'a':
+      allrank = true;
       break;
     case 'q':
       theInfo.quiet = true;
@@ -35,17 +39,24 @@ void getRank(int argc, char *argv[]) {
 
   // get the rank abbreviation map
   TaxaRank *rank = TaxaRank::create();
-  vector<string> ranklist = {"kingdom", "phylum", "class",  "order",
-                             "family",  "genus",  "species"};
-  vector<string> prefix{"super", "", "sub"};
-
   ofstream ofs(outfile);
-  for (auto nm : ranklist) {
-    for (auto pf : prefix) {
-      string name = pf + nm;
-      auto iter = rank->rankmap.find(name);
-      if (iter != rank->rankmap.end()) {
-        ofs << iter->first << "\t" << iter->second << endl;
+
+  if (allrank) {
+    for (auto &rk : rank->rankmap) {
+      ofs << rk.first << "\t" << rk.second << endl;
+    }
+  } else {
+    vector<string> ranklist = {"kingdom", "phylum", "class",  "order",
+                               "family",  "genus",  "species"};
+    vector<string> prefix{"super", "", "sub"};
+
+    for (auto nm : ranklist) {
+      for (auto pf : prefix) {
+        string name = pf + nm;
+        auto iter = rank->rankmap.find(name);
+        if (iter != rank->rankmap.end()) {
+          ofs << iter->first << "\t" << iter->second << endl;
+        }
       }
     }
   }
@@ -57,6 +68,7 @@ void rankUsage(string &program) {
        << program << "\n"
        << " [ -o ranklist.txt ]  Output name list file,\n"
        << "                      default: ranklist.txt\n"
+       << " [ -a ]               Output all inbuilt ranks\n"
        << " [ -q ]               Run command in quiet mode\n"
        << " [ -h ]               Display this information\n"
        << endl;
